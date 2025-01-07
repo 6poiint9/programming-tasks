784a92b562faa7a674fe242383fe7f840895d7c8
sixpoint nine <james532@tutamail.com>
2025-01-07 13:01:00 +0100

    stuff

--- Labor/Labor8/l8_a7.c
+++ Labor/Labor8/l8_a7.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
-// Primzahl kontrolle
+// Recursive function to check if a number is prime
+int isPrimeHelper(int num, int divisor);
 int prime(int num);
 
-int main()
-{
-  int num;
-  scanf("%d", &num);
-  int res = int prime(num);
-  printf("%d", res); 
-  return 0;
+int main() {
+    int num;
+    scanf("%d", &num);
+    int res = prime(num);
+    // Ausgabe
+    printf("%d\n", res);  
+    return 0;
 }
 
+// Wrapper function for checking if a number is prime
 int prime(int num) {
+    if (num <= 1) {  // Numbers less than or equal to 1 are not prime
+        return 0;
+    }
+    return isPrimeHelper(num, num / 2);  // Start checking divisors from num/2
+}
 
- if 
+// Recursive helper function
+int isPrimeHelper(int num, int divisor) {
+    if (divisor == 1) {  // Base case: no divisors found, number is prime
+        return 1;
+    }
+    if (num % divisor == 0) {  // If num is divisible by divisor, it's not prime
+        return 0;
+    }
+    return isPrimeHelper(num, divisor - 1);  // Check next smaller divisor
 }
 
